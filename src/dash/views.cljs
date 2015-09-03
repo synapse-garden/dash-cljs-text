21d6021f12a34b1a1cab7461ebd5b16df2229e27
Kevin Weber <kevin@synapsegarden.net>
2015-09-03 13:43:14 -0400

    Log-in basic elements on a DOM level

--- src/dash/views.cljs
+++ src/dash/views.cljs
@@ -7,14 +7,26 @@
   (reify om/IRender (render [_]
     (dom/div #js {:id "login-container"}
       (dom/h3 "Log-in to Dash")
-      (dom/form
-        (dom/p "Email > ")
+      (dom/form {:class "login-form"}
+        (dom/text #js {:class "login-label"} "Email")
         (dom/input #js {:type "text" :name "email"})
-        (dom/br)
-        (dom/p "Password > ")
+        (dom/text #js {:class "login-label"} "Password")
         (dom/input #js {:type "password" :name "pass"})
-        (dom/br)
         (dom/input #js {:type "submit" :value "Log-in"}))))))
+
+(defn login-table-view []
+  (reify om/IRender (render [_]
+    (dom/div #js {:id "login-container"}
+      (dom/h3 "Log-in to Dash")
+      (dom/form {:class "login-form"}
+        (dom/table {:class "login-table"}
+          (dom/tr #js {:class "login-labels"}
+            (dom/td "Email")
+            (dom/td "Password"))
+          (dom/tr #js {:class "login-elements"}
+            (dom/td (dom/input #js {:type "text" :name "email"}))
+            (dom/td (dom/input #js {:type "password" :name "pass"}))
+            (dom/td (dom/input #js {:type "submit" :value "Log-in"})))))))))
 
 (defn login-test-view []
   (reify om/IRender (render [_]
