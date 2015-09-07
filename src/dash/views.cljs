e75dfaff71e940a4142090f83cc06613c3f459e9
Kevin Weber <kevin@synapsegarden.net>
2015-09-07 12:57:23 -0400

    WIP using cursors

--- src/dash/views.cljs
+++ src/dash/views.cljs
@@ -4,42 +4,48 @@
             [dash.util :as util]
             [dash.core :as core]))
 
-(defn view-switcher [state]
+(defn change-view [cursor view-id]
+  (om/transact! cursor [0] view-id))
+
+(defn view-switcher [ cursor ]
   (reify om/IRender (render [_]
-    (let [current-view (:view state)]
+    (let [current-view cursor]
     (dom/button (if-not (= current-view 0)
-                #js {:className "view-switcher" :onClick (core/upsert-view state 0)}
+                #js {:className "view-switcher" :onClick (change-view cursor 0)}
                 #js {:className "view-switcher-disabled" :onClick nil})
                 "View A")
     (dom/button (if-not (= current-view 1)
-                #js {:className "view-switcher" :onClick (core/upsert-view state 1)}
+                #js {:className "view-switcher" :onClick (change-view cursor 1)}
                 #js {:className "view-switcher-disabled" :onClick nil})
                 "View B")
     (dom/button (if-not (= current-view 2)
-                #js {:className "view-switcher" :onClick (core/upsert-view state 2)}
+                #js {:className "view-switcher" :onClick (change-view cursor 2)}
                 #js {:className "view-switcher-disabled" :onClick nil})
                 "View C")
     ))))
 
-(defn view-a [state]
+(defn view-a [cursor]
   (reify om/IRender (render [_]
     (dom/h2 nil "This is View A")
+    (dom/h3 nil (str "(Also known in the atom as View " (get cursor :view)))
     (dom/p nil "AAAAAAAAAAAA")
-    (om/build view-switcher (state)))))
+    (om/build view-switcher (get cursor :view)))))
 
-(defn view-b [state]
+(defn view-b [cursor]
   (reify om/IRender (render [_]
     (dom/h2 nil "This is View B")
+    (dom/h3 nil (str "(Also known in the atom as View " (get cursor :view)))
     (dom/p nil "BBBBBBBBBBBB")
-    (om/build view-switcher (state)))))
+    (om/build view-switcher (get cursor :view)))))
 
-(defn view-c [state]
+(defn view-c [cursor]
   (reify om/IRender (render [_]
     (dom/h2 nil "This is View C")
+    (dom/h3 nil (str "(Also known in the atom as View " (get cursor :view)))
     (dom/p nil "CCCCCCCCCCCC")
-    (om/build view-switcher (state)))))
+    (om/build view-switcher (get cursor :view)))))
 
-(defn views-view [state owner] ;META
+(defn views-view [cursor owner] ;META
   (reify om/IRender (render [_]
     (dom/div #js {:id "test-container"}
       (dom/h3 nil "Login Test View")
