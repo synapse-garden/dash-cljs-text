3ff2c5dfa92ee9b9279d19dddee1b8268897f1b8
Kevin Weber <kevin@synapsegarden.net>
2015-09-08 18:22:58 -0400

    WIP cursors

--- src/dash/views.cljs
+++ src/dash/views.cljs
@@ -5,69 +5,52 @@
             [dash.core :as core]
             ))
 
-(def view {:cur-view 0})
+(defn eval-view [cursor]
+  (.log js/console (str "Evaluate View in atom via cursor: " (get-in cursor [:view 0]) ) ) )
 
-(defn view-switcher [state]
+(defn view-switcher [cursor]
   (reify om/IRender (render [_]
-    (let [current-view (:view state)]
+    (let [current-view (get cursor 0)]
 
-    (dom/button #js {:className "view-switcher" :disabled false }
-      (if (= current-view 0)
-        #js {:disabled true}
-        #js {:onClick #(dash.core/upsert-view state 0)})
-      (str "View " current-view))
+    (for [v [0 1]]
+      (dom/button #js {:className "view-switcher" :disabled false }
+        (if (= current-view v)
+          #js {:disabled true }
+          #js {:onClick #(om/transact! (get cursor :view) v)})
+        (str "View " current-view))
+    )))))
 
-    (dom/button #js {:className "view-switcher" :disabled false }
-      (if (= current-view 1)
-        #js {:disabled true}
-        #js {:onClick #(dash.core/upsert-view state 1)})
-      (str "View " current-view))
-
-    (dom/button #js {:className "view-switcher" :disabled false }
-      (if (= current-view 2)
-        #js {:disabled true}
-        #js {:onClick #(dash.core/upsert-view state 2)})
-      (str "View " current-view))
-
-    ; (for [v (range 0 2)
-    ;   (dom/button #js {:className "view-switcher" :disabled false }
-    ;   (if (= current-view v)
-    ;     #js {:disabled true }
-    ;     #js {:onClick (dash.core/upsert-view state v)})
-    ;   (str "View " current-view))])
-    ))))
-
-(defn view-a [state]
+(defn view-a [cursor]
   (reify om/IRender (render [_]
     (dom/div nil
       (dom/h1 nil "This is View A")
-      (dom/h3 nil (str "(Also known in the atom as View " (str (:view state)) ")"))
+      (dom/h3 nil (str "(Also known in the atom as View " (str (get cursor 0)) ")"))
       (dom/p nil "AAAAAAAAAAAA")
       (dom/button #js {:className "test-button" :onClick #(.log js/console "Hello!")} "Hello Test")
-      (om/build-all view-switcher state)
+      (om/build-all view-switcher cursor)
       ))))
 
-(defn view-b [state]
+(defn view-b [cursor]
   (reify om/IRender (render [_]
     (dom/div nil
       (dom/h1 nil "This is View B")
-      (dom/h3 nil (str "(Also known in the atom as View " (str (:view state)) ")"))
+      (dom/h3 nil (str "(Also known in the Atom as View " (str (get cursor 0)) ")"))
       (dom/p nil "BBBBBBBBBBBB")
       (dom/button #js {:className "test-button" :onClick #(.log js/console "Hello!")} "Hello Test")
-      (om/build-all view-switcher state)
+      (om/build-all view-switcher cursor)
       ))))
 
-(defn view-c [state]
+(defn simple-view [cursor]
   (reify om/IRender (render [_]
     (dom/div nil
-      (dom/h1 nil "This is View C")
-      (dom/h3 nil (str "(Also known in the atom as View " (str (:view state)) ")"))
-      (dom/p nil "CCCCCCCCCCCC")
-      (dom/button #js {:className "test-button" :onClick #(.log js/console "Hello!")} "Hello Test")
-      (om/build-all view-switcher state)
-      ))))
+      (dom/h2 nil "Simple Cursor Test View")
+      (eval-view cursor)
+      (dom/p nil (str "I am attempting to retrieve a value from the atom via cursors.\n
+                  The value I'm expecting is 0. The value is " (get-in cursor [:view 0])))
+      (eval-view cursor)
+    ))))
 
-(defn views-view [state owner] ;META
+(defn views-view [cursor owner] ;META
   (reify om/IRender (render [_]
     (dom/div #js {:id "test-container"}
       (dom/h3 nil "Login Test View")
