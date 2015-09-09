6be50996d25687caa3b0e2d63fe12988d6d7109f
Kevin Weber <kevin@synapsegarden.net>
2015-09-09 02:06:31 -0400

    Progress on view switcher. Seems to only render last button so far

--- src/dash/views.cljs
+++ src/dash/views.cljs
@@ -7,23 +7,29 @@
 
 (defn view-switcher [cursor]
   (reify om/IRender (render [_]
-    (let [current-view (get cursor 0)]
+    (let [current-view (first cursor)]
 
-    (doseq [v [0 1]]
-      (dom/button #js {:className "view-switcher" :disabled false }
-        (if (= current-view v)
-          #js {:disabled true }
-          #js {:onClick #(om/transact! [:view 0] v)})
-        (str "View " current-view)))
+    (dom/button #js {:className "test-button" :onClick #(.log js/console "Hello!")} "Hello Test")
+
+    (dom/button #js {:className "view-switcher"}
+      (if (= current-view 0)
+        #js {:disabled true}
+        #js {:disabled false :onClick #(om/transact! [:view 0] 0)})
+      (str "View A (" current-view ")"))
+
+    (dom/button #js {:className "view-switcher" :disabled false }
+      (if (= current-view 1)
+        #js {:disabled true}
+        #js {:disabled false :onClick #(om/transact! [:view 0] 1)})
+      (str "View B (" current-view ")"))
     ))))
 
 (defn view-a [cursor]
   (reify om/IRender (render [_]
     (dom/div nil
       (dom/h1 nil "This is View A")
-      (dom/h3 nil (str "(Also known in the atom as View " (str (get-in cursor [:view 0])) ")"))
-      (dom/p nil "AAAAAAAAAAAA")
-      (dom/button #js {:className "test-button" :onClick #(.log js/console "Hello!")} "Hello Test")
+      (dom/h4 nil (str "(Also known in the atom as View " (str (get-in cursor [:view 0])) ")"))
+      (dom/p nil "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
       (om/build view-switcher (:view cursor))
       ))))
 
@@ -31,18 +37,17 @@
   (reify om/IRender (render [_]
     (dom/div nil
       (dom/h1 nil "This is View B")
-      (dom/h3 nil (str "(Also known in the Atom as View " (str (get-in cursor [:view 0])) ")"))
-      (dom/p nil "BBBBBBBBBBBB")
-      (dom/button #js {:className "test-button" :onClick #(.log js/console "Hello!")} "Hello Test")
+      (dom/h4 nil (str "(Also known in the Atom as View " (str (get-in cursor [:view 0])) ")"))
+      (dom/p nil "BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB")
       (om/build view-switcher (:view cursor))
       ))))
 
 (defn simple-view [cursor]
   (reify om/IRender (render [_]
     (dom/div nil
-      (dom/h2 nil "Simple Cursor Test View")
+      (dom/h1 nil "Simple Cursor Test View")
       (dom/p nil (str "I am attempting to retrieve a value from the atom via cursors.
-                      The value I'm expecting is 0. The value I find is " (get-in cursor [:view 0])))
+                      The value I'm expecting is 0. The value I find is " (get-in cursor [:view 0]) "."))
     ))))
 
 (defn views-view [cursor owner] ;META
