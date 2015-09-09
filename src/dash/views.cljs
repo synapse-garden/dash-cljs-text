1487d67e01444928dcc457873d5f952cdb7358aa
Kevin Weber <kevin@synapsegarden.net>
2015-09-08 23:21:49 -0400

    Found erasing atom culprit -- the 'fetch-updates' function

--- src/dash/views.cljs
+++ src/dash/views.cljs
@@ -5,49 +5,44 @@
             [dash.core :as core]
             ))
 
-(defn eval-view [cursor]
-  (.log js/console (str "Evaluate View in atom via cursor: " (get-in cursor [:view 0]) ) ) )
-
 (defn view-switcher [cursor]
   (reify om/IRender (render [_]
     (let [current-view (get cursor 0)]
 
-    (for [v [0 1]]
+    (doseq [v [0 1]]
       (dom/button #js {:className "view-switcher" :disabled false }
         (if (= current-view v)
           #js {:disabled true }
-          #js {:onClick #(om/transact! (get cursor :view) v)})
-        (str "View " current-view))
-    )))))
+          #js {:onClick #(om/transact! [:view 0] v)})
+        (str "View " current-view)))
+    ))))
 
 (defn view-a [cursor]
   (reify om/IRender (render [_]
     (dom/div nil
       (dom/h1 nil "This is View A")
-      (dom/h3 nil (str "(Also known in the atom as View " (str (get cursor 0)) ")"))
+      (dom/h3 nil (str "(Also known in the atom as View " (str (get-in cursor [:view 0])) ")"))
       (dom/p nil "AAAAAAAAAAAA")
       (dom/button #js {:className "test-button" :onClick #(.log js/console "Hello!")} "Hello Test")
-      (om/build-all view-switcher cursor)
+      (om/build view-switcher (:view cursor))
       ))))
 
 (defn view-b [cursor]
   (reify om/IRender (render [_]
     (dom/div nil
       (dom/h1 nil "This is View B")
-      (dom/h3 nil (str "(Also known in the Atom as View " (str (get cursor 0)) ")"))
+      (dom/h3 nil (str "(Also known in the Atom as View " (str (get-in cursor [:view 0])) ")"))
       (dom/p nil "BBBBBBBBBBBB")
       (dom/button #js {:className "test-button" :onClick #(.log js/console "Hello!")} "Hello Test")
-      (om/build-all view-switcher cursor)
+      (om/build view-switcher (:view cursor))
       ))))
 
 (defn simple-view [cursor]
   (reify om/IRender (render [_]
     (dom/div nil
       (dom/h2 nil "Simple Cursor Test View")
-      (eval-view cursor)
-      (dom/p nil (str "I am attempting to retrieve a value from the atom via cursors.\n
-                  The value I'm expecting is 0. The value is " (get-in cursor [:view 0])))
-      (eval-view cursor)
+      (dom/p nil (str "I am attempting to retrieve a value from the atom via cursors.
+                      The value I'm expecting is 0. The value I find is " (get-in cursor [:view 0])))
     ))))
 
 (defn views-view [cursor owner] ;META
