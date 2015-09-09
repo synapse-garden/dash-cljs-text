3e42685c5bab999c059999e66a88f82506808a5a
Kevin Weber <kevin@synapsegarden.net>
2015-09-09 02:35:44 -0400

    Buttons correctly switch views and interact with the atom through cursors

--- src/dash/views.cljs
+++ src/dash/views.cljs
@@ -8,21 +8,19 @@
 (defn view-switcher [cursor]
   (reify om/IRender (render [_]
     (let [current-view (first cursor)]
-
-    (dom/button #js {:className "test-button" :onClick #(.log js/console "Hello!")} "Hello Test")
-
-    (dom/button #js {:className "view-switcher"}
-      (if (= current-view 0)
-        #js {:disabled true}
-        #js {:disabled false :onClick #(om/transact! [:view 0] 0)})
-      (str "View A (" current-view ")"))
-
-    (dom/button #js {:className "view-switcher" :disabled false }
-      (if (= current-view 1)
-        #js {:disabled true}
-        #js {:disabled false :onClick #(om/transact! [:view 0] 1)})
-      (str "View B (" current-view ")"))
-    ))))
+    (dom/div nil
+        (dom/button
+          (if (= current-view 0)
+            #js {:className "view-switcher" :disabled true}
+            #js {:className "view-switcher" :onClick #(om/update! cursor [0] 0)})
+          (str "View A (" current-view ")"))
+    
+        (dom/button
+          (if (= current-view 1)
+            #js {:className "view-switcher" :disabled true}
+            #js {:className "view-switcher" :onClick #(om/update! cursor [0] 1)})
+          (str "View B (" current-view ")"))
+    )))))
 
 (defn view-a [cursor]
   (reify om/IRender (render [_]
@@ -37,7 +35,7 @@
   (reify om/IRender (render [_]
     (dom/div nil
       (dom/h1 nil "This is View B")
-      (dom/h4 nil (str "(Also known in the Atom as View " (str (get-in cursor [:view 0])) ")"))
+      (dom/h4 nil (str "(Also known in the atom as View " (str (get-in cursor [:view 0])) ")"))
       (dom/p nil "BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB")
       (om/build view-switcher (:view cursor))
       ))))
