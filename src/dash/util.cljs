ea0d0a2c072e6e15e28563a202aee7fb39164c7b
Kevin Weber <kevin@synapsegarden.net>
2015-09-13 00:53:17 -0400

    Functioning canvas element using the Quil library

--- src/dash/util.cljs
+++ src/dash/util.cljs
@@ -36,3 +36,28 @@
 (defn uri [root path]
   "Construct a URI given a root and path"
   (str root path))
+
+(defn lerp [a b x]
+  "Linear interpolation between two values"
+  (+ a (* x (- b a))))
+
+(defn remap [aa ab ba bb value]
+  "Remaps a number in one range to another range"
+   (lerp ba bb (/ (- value aa) (- ab aa)))
+)
+
+(defn normalize [a b x]
+  "Normalizes a number from an arbitrary range to 0-1"
+  (remap a b 0 1 x))
+
+(defn soft-clamp
+  "Softly clamps all values to between -1 and 1"
+  ([x tightness] (/ (Math/atan (* x (+ tightness 1))) Math/PI))
+  ([x] (soft-clamp x 0))
+  )
+
+(defn soft-clamp-pos
+  "Softly clamps all values to between 0 and 1"
+  ([x tightness] (+ (* (soft-clamp x tightness) 0.5) 1))
+  ([x] (soft-clamp-pos x 0))
+  )
